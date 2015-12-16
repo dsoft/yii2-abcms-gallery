012cb16cf0180beef0fedec904df1b58ff2b0bc3
ta-king <ta-king@users.noreply.github.com>
2015-12-16 16:26:34 +0200

    Adding tables migrations for gallery_album and gallery_image

---
+++ migrations/m151216_114228_create_gallery_album.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m151216_114228_create_gallery_album extends Migration
+{
+
+    public function up()
+    {
+        $this->createTable('gallery_album', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(100)->notNull(),
+            'categoryId' => $this->integer()->notNull(),
+            'active' => $this->boolean()->defaultValue(1)->notNull(),
+            'deleted' => $this->boolean()->defaultValue(0)->notNull(),
+            'ordering' => $this->integer()->defaultValue(1)->notNull(),
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('gallery_album');
+    }
+
+}
---
+++ migrations/m151216_114713_create_gallery_image.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m151216_114713_create_gallery_image extends Migration
+{
+
+    public function up()
+    {
+        $this->createTable('gallery_image', [
+            'id' => $this->primaryKey(),
+            'albumId' => $this->integer()->notNull(),
+            'image' => $this->string(50)->notNull(),
+            'active' => $this->boolean()->defaultValue(1)->notNull(),
+            'deleted' => $this->boolean()->defaultValue(0)->notNull(),
+            'ordering' => $this->integer()->defaultValue(1)->notNull(),
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('gallery_image');
+    }
+
+}
